5aa831e1878cc49eb5bc8b3e4f6686f0b69a011b
Vsevolod Skripnik <vsevolod_skripnik@protonmail.com>
2021-01-09 19:47:00 +0300

    Delete the tests

--- test/Paragraph.spec.js
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils'
-import Paragraph from '@/components/Paragraph.vue'
-
-describe('Paragraph', () => {
-  test('is a Vue instance', () => {
-    const wrapper = mount(Paragraph)
-    expect(wrapper.vm).toBeTruthy()
-  })
-})
