ca5d2ce6c549e78d79997630ef47ee19cefad7f5
Vsevolod Skripnik <vsevolod_skripnik@protonmail.com>
2021-01-16 16:40:56 +0300

    16 pages in to-self

---
+++ components/book/ToSelf/Thought.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <Heading3 :inner="title" />
+    <Paragraph :inner="text" no-indent="true" />
+    <Spacer size="one-line" />
+  </div>
+</template>
+
+<script>
+import Heading3 from '@/components/Heading3.vue'
+import Paragraph from '@/components/Paragraph.vue'
+import Spacer from '@/components/Spacer.vue'
+
+export default {
+  name: 'Thought',
+  components: {
+    Heading3,
+    Paragraph,
+    Spacer,
+  },
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+    text: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
