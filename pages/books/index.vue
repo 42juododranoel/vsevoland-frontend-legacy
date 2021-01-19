0509f0c464cc40dadd211f1ec67dc231e9677bec
Vsevolod Skripnik <vsevolod_skripnik@protonmail.com>
2021-01-19 21:45:51 +0300

    Everything better

---
+++ pages/books/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <NuxtLink to="/books/to-self">
+        <div class="col-12">
+          …
+        </div>
+        <div class="col-4">
+          <Heading3 inner="К себе" />
+        </div>
+      </NuxtLink>
+    </div>
+    <div class="row">
+      <div class="col-4">
+        <NuxtLink to="/books/the-dragonrider-and-the-witch">
+          <Heading3 inner="Всадник и Ведьма" />
+        </NuxtLink>
+      </div>
+      <div class="col-12">
+        <NuxtLink to="/books/the-dragonrider-and-the-witch">…</NuxtLink>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+}
+</script>
