d555e1f5a097f62d4f3f5fd367afd7e563ab0ed7
Vsevolod Skripnik <vsevolod_skripnik@protonmail.com>
2021-11-10 20:08:57 +0300

    + Whatever lets you sleep at night

--- pages/books/zeroism.vue
+++ pages/books/zeroism.vue
@@ -34,6 +34,11 @@ const contents = {
               text:
                 'Whenever you find yourself building morals, defining good and evil, hell and heaven, god and devil, black and white, wrong and right — remind yourself how awful it smells, how it’s all imaginary castles, fantasies, lies, deception. There are no such things among the stars. Remind yourself to not believe in greater meanings, but to believe in opinions, intents, actions, consequences. Let your actions be born out of no high matter, let yourself be the way you are — chaotic, unpredictable, sparks around the bonfire, here and now.',
             },
+            {
+              title: 'Whatever lets you sleep at night',
+              text:
+                'Well, there are people who are damn rich, people who are damn hot, people who are praised for their personality and relationships with other people, there are people of many virtues, people whose deeds are piercing heavens and shining like stars — and then there is you, the observer, the judge. How come it you are so hard on yourself? Well, being a human, it’s natural to have human perspectives, but look the other way, see with the eyes of the nature: you are living, isn’t that enough? Aren’t you experiencing the same way from point B to point D as all other people do? Surely things are different for everyone, but at the very core it’s all the same existing, really.',
+            },
           ],
         },
         {
